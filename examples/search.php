289826347b94c1befe8c57537e57a908e67c95c7
cherry <cherryred5959@gmail.com>
2019-02-16 21:52:06 +0900

    예제 소스 수정

--- examples/search.php
+++ examples/search.php
@@ -2,8 +2,5 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-try {
-    dump((new \InstagramTagScraper\Instagram(new \GuzzleHttp\Client()))->search('iphone'));
-} catch (\GuzzleHttp\Exception\GuzzleException $e) {
-    dump($e);
-}
+/** @noinspection PhpUnhandledExceptionInspection*/
+(new \InstagramTagScraper\Instagram(new \GuzzleHttp\Client()))->search('iphone');
