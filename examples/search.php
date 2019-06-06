5f91cf4293ae182200304ca243be9555561872b8
cherry <cherryred5959@gmail.com>
2019-06-06 12:33:47 +0900

    예제 소스 수정.

--- examples/search.php
+++ examples/search.php
@@ -6,4 +6,4 @@ use GuzzleHttp\Client;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 /** @noinspection PhpUnhandledExceptionInspection*/
-(new Instagram(new Client()))->search('iphone');
+dump((new Instagram(new Client()))->search('hello'));
